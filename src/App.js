b314baa0753b9f6ae1446849a4376258a21b83c1
agenttim <agent_tim@mail.ru>
2023-08-07 12:25:35 +0500

    сделал разделы прочие разделы

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
 
 function App() {
     return (
         <div className="App">
             <Home></Home>
             <About></About>
+            <Work></Work>
+            <Testimonial></Testimonial>
+            <Contact></Contact>
+            <Footer></Footer>
+
+
         </div>
     );
 }
