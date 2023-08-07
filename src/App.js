41a04f54867fc4dfc33aa2a9e1b611e6a8153aeb
agenttim <agent_tim@mail.ru>
2023-08-07 12:03:08 +0500

    сделал разделы home и about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from "./Components/Home";
+import About from "./Components/About";
 
 function App() {
-  return (
-      <div className="App">
-        <Home></Home>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Home></Home>
+            <About></About>
+        </div>
+    );
 }
 
 export default App;
